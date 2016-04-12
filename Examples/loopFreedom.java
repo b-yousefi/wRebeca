350d0c1689be5e16c39c32a7e9beaefc36b0dd66
Behnaz <behyousefi@yahoo.com>
2016-04-12 05:14:56 +0430

    Added files via upload

---
+++ Examples/loopFreedom.java
@@ -0,0 +1,95 @@
+//Invariant
+public Boolean loopFreedom(glState gl, Object[] pars)
+{
+	boolean loop;
+
+	int src =0;
+	int[] seen = null;
+	if (pars != null)
+	{
+		int i = 0;
+		if (pars[i] != null) src = (int)pars[i];
+		i++;
+		if (pars[i] != null) seen = (int[])pars[i];
+	}
+
+	{
+
+		loop = false;
+
+
+
+		for (int id = 0; id < gl.getStates().size(); id++)
+		{
+			for (int des = 0; des < gl.getStates().size(); des++)
+			{
+
+
+				seen = new int[gl.getStates().size()];
+				for (int i = 0; i < gl.getStates().size(); i++)
+				{
+					seen[i] = -1;
+				}
+				loop = looop(gl, des, id, 0, seen);
+				if (loop)
+					break;
+			}
+			if (loop)
+				break;
+		}
+		if (loop)
+		{
+			System.out.println("Loop is found!!!!!!!!!!!");
+			return true;
+		}
+		else
+			return false;
+	}
+
+}
+boolean looop(glState gl, int des, int src, int indx, int[] seen)
+{
+
+	int nextHop = -1;
+	boolean loop = false;
+
+	Node curNode = (Node) gl.getStates().get(src) ;
+	seen[indx] = curNode.getId();
+	indx++;
+	for (int k = 0; k < gl.getStates().size(); k++)
+	{
+		nextHop = curNode.getnhop()[des][k];
+		if (nextHop == curNode.getId())
+			break;
+
+		for (int j = 0; j < indx; j++)
+		{
+			if (nextHop == des)
+				break;
+			if (seen[j] == nextHop)
+			{
+				loop = true;
+				break;
+			}
+		}
+		if (loop)
+			return true;
+		else
+		{
+			//src = nextHop;
+			if (nextHop == -1 || nextHop == des)
+			{// return false;
+			}
+			else
+				if (looop(gl, des, nextHop, indx, seen))
+				{
+					loop = true;
+					break;
+				}
+		}
+	}
+	if (loop)
+		return true;
+	else
+		return false;
+}
